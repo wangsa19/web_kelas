f195e85ac8cef1f3b82a283de0420395f2058100
Fauzaaannn <fauzanabderrasheed@gmail.com>
2024-06-09 14:06:40 +0700

    profile page - responsive hp belum

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,52 +1,100 @@
 @extends('layouts.app')
 @section('content')
-    {{-- Judul e style en sembarang --}}
-    <h1 class="font-thin flex justify-center text-2xl pb-5 pt-10">Data Kelas D3 IT A</h1>
-    {{-- Search e kek ono animasi yo sembarang,pokok e iso search arek sg pengen di stalker --}}
-    <input type="search" placeholder="cari nama" class="float-right border-2 border-black mr-5">
-    {{-- List anggota kelas,tampilno nama kro nrp tok ae.Nah tugas mu gae o 1 form gae nampillno data e arek iku
-      sg isine nama lengkap,nrp,foto,alamat asli + domisili,kro deskripsi ne arek iku.Ntah njelasno isok bahasa opo ae
-      ta njelasno porto e wes sembuarang.Ngkok iki gae API ae,nah ngkok delok detail e cukup get by ID tok --}}
-    <div id="anggota" class="pt-20 px-10">
-        <div id="1">
-            <ul class="flex justify-between">
-                <li>
-                    <div id="komting" class="border-2 border-black">
-                        <img src="" width="">
-                        <h1 class="text-lg">Nama</h1>
-                        <p class="text-sm">Nrp</p>
-                    </div>
-                </li>
-                <li>
-                    <div id="komting" class="border-2 border-black">
-                        <img src="" width="">
-                        <h1 class="text-lg">Nama</h1>
-                        <p class="text-sm">Nrp</p>
-                    </div>
-                </li>
-                <li>
-                    <div id="komting" class="border-2 border-black">
-                        <img src="" width="">
-                        <h1 class="text-lg">Nama</h1>
-                        <p class="text-sm">Nrp</p>
-                    </div>
-                </li>
-                <li>
-                    <div id="komting" class="border-2 border-black">
-                        <img src="" width="">
-                        <h1 class="text-lg">Nama</h1>
-                        <p class="text-sm">Nrp</p>
-                    </div>
-                </li>
-                <li>
-                    <div id="komting" class="border-2 border-black">
-                        <img src="" width="">
-                        <h1 class="text-lg">Nama</h1>
-                        <p class="text-sm">Nrp</p>
-                    </div>
-                </li>
-            </ul>
+
+    <section id="hero">
+        <div class="max-w-full flex flex-wrap mt-20 md:mt-20 lg:mt-24 px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20 bg-white gap-y-4 sm:gap-y-4 md:gap-y-6 lg:gap-y-0 lg:gap-x-4 xl:gap-x-12">
+        <div class="w-full lg:w-[90%] xl:w-[100%]">
+            <div class="w-full flex flex-row">
+                <div class="w-full gap-y-6 sm:gap-y-8 md:gap-y-10 lg:gap-y-12 xl:gap-y-8">
+                    <h2 class="font-semibold sm:text-3xl md:text-[40px] lg:text-6xl">Kenalan Yuk !</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="w-full lg:mt-12 sm:mt-4 md:mt-6 flex justify-center">
+            <div class="flex justify-center">
+                <picture>
+                    <source srcset="gif/komting.webp" type="image/webp">
+                    <img src="gif/komting.jpg" alt=""
+                        class="w-[200px] lg:w-[300px] h-[200px] lg:h-[300px] rounded-full">
+                </picture>
+            </div>
+        </div>
+        <div class="w-full sm:mt-2 md:mt-4 lg:mt-8">
+            <div class="flex justify-center">
+                <p class="text-center">Ragil Ridho Saputra</p>
+            </div>
+            <div class="flex justify-center">
+                <p class="text-center mt-4">Komisi Tingkat D3 Teknik Informatika A 2023</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="profiles">
+    <div class="w-full grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xl:px-20 lg:px-16 md:px-12 sm:px-0 mx-auto items-center justify-center mt-10 mb-16">
+        <div class="border snap-start xl:h-[342px] lg:h-72 md:h-64 sm:h-40 list-none bg-blue-100 rounded-xl flex cursor-pointer items-center justify-end flex-col text-primary pt-2 px-2 pb-4 xl:my-2 md:my-2 mx-2">
+            <div class="bg-white w-full xl:h-full lg:h-full md:h-[190px] sm:h-32 rounded-xl mb-2 flex cursor-pointer items-center justify-center flex-col p-4 text-primary;">
+                <span class="text-[#6a6d78] text-base sm:text-lg lg:text-xl text-center mb-auto mt-36">Sembarang</span>
+            </div>
+            <h2 class="font-medium text-lg lg:text-2xl mt-1">Ragil</h2>
+        </div>
+        <div class="border snap-start xl:h-[342px] lg:h-72 md:h-64 sm:h-40 list-none bg-blue-100 rounded-xl flex cursor-pointer items-center justify-end flex-col text-primary pt-2 px-2 pb-4 xl:my-2 md:my-2 mx-2">
+            <div class="bg-white w-full xl:h-full lg:h-full md:h-[190px] sm:h-32 rounded-xl mb-2 flex cursor-pointer items-center justify-center flex-col p-4 text-primary;">
+                <span class="text-[#6a6d78] text-base sm:text-lg lg:text-xl text-center mb-auto mt-36">Sembarang</span>
+            </div>
+            <h2 class="font-medium text-lg lg:text-2xl mt-1">Ragil</h2>
+        </div>
+        <div class="border snap-start xl:h-[342px] lg:h-72 md:h-64 sm:h-40 list-none bg-blue-100 rounded-xl flex cursor-pointer items-center justify-end flex-col text-primary pt-2 px-2 pb-4 xl:my-2 md:my-2 mx-2">
+            <div class="bg-white w-full xl:h-full lg:h-full md:h-[190px] sm:h-32 rounded-xl mb-2 flex cursor-pointer items-center justify-center flex-col p-4 text-primary;">
+                <span class="text-[#6a6d78] text-base sm:text-lg lg:text-xl text-center mb-auto mt-36">Sembarang</span>
+            </div>
+            <h2 class="font-medium text-lg lg:text-2xl mt-1">Ragil</h2>
+        </div>
+        <div class="border snap-start xl:h-[342px] lg:h-72 md:h-64 sm:h-40 list-none bg-blue-100 rounded-xl flex cursor-pointer items-center justify-end flex-col text-primary pt-2 px-2 pb-4 xl:my-2 md:my-2 mx-2">
+            <div class="bg-white w-full xl:h-full lg:h-full md:h-[190px] sm:h-32 rounded-xl mb-2 flex cursor-pointer items-center justify-center flex-col p-4 text-primary;">
+                <span class="text-[#6a6d78] text-base sm:text-lg lg:text-xl text-center mb-auto mt-36">Sembarang</span>
+            </div>
+            <h2 class="font-medium text-lg lg:text-2xl mt-1">Ragil</h2>
+        </div>
+        <div class="border snap-start xl:h-[342px] lg:h-72 md:h-64 sm:h-40 list-none bg-blue-100 rounded-xl flex cursor-pointer items-center justify-end flex-col text-primary pt-2 px-2 pb-4 xl:my-2 md:my-2 mx-2">
+            <div class="bg-white w-full xl:h-full lg:h-full md:h-[190px] sm:h-32 rounded-xl mb-2 flex cursor-pointer items-center justify-center flex-col p-4 text-primary;">
+                <span class="text-[#6a6d78] text-base sm:text-lg lg:text-xl text-center mb-auto mt-36">Sembarang</span>
+            </div>
+            <h2 class="font-medium text-lg lg:text-2xl mt-1">Ragil</h2>
+        </div>
+
+        <div class="border snap-start xl:h-[342px] lg:h-72 md:h-64 sm:h-40 list-none bg-blue-100 rounded-xl flex cursor-pointer items-center justify-end flex-col text-primary pt-2 px-2 pb-4 xl:my-2 md:my-2 mx-2">
+            <div class="bg-white w-full xl:h-full lg:h-full md:h-[190px] sm:h-32 rounded-xl mb-2 flex cursor-pointer items-center justify-center flex-col p-4 text-primary;">
+                <span class="text-[#6a6d78] text-base sm:text-lg lg:text-xl text-center mb-auto mt-36">Sembarang</span>
+            </div>
+            <h2 class="font-medium text-lg lg:text-2xl mt-1">Ragil</h2>
+        </div>
+        <div class="border snap-start xl:h-[342px] lg:h-72 md:h-64 sm:h-40 list-none bg-blue-100 rounded-xl flex cursor-pointer items-center justify-end flex-col text-primary pt-2 px-2 pb-4 xl:my-2 md:my-2 mx-2">
+            <div class="bg-white w-full xl:h-full lg:h-full md:h-[190px] sm:h-32 rounded-xl mb-2 flex cursor-pointer items-center justify-center flex-col p-4 text-primary;">
+                <span class="text-[#6a6d78] text-base sm:text-lg lg:text-xl text-center mb-auto mt-36">Sembarang</span>
+            </div>
+            <h2 class="font-medium text-lg lg:text-2xl mt-1">Ragil</h2>
+        </div>
+        <div class="border snap-start xl:h-[342px] lg:h-72 md:h-64 sm:h-40 list-none bg-blue-100 rounded-xl flex cursor-pointer items-center justify-end flex-col text-primary pt-2 px-2 pb-4 xl:my-2 md:my-2 mx-2">
+            <div class="bg-white w-full xl:h-full lg:h-full md:h-[190px] sm:h-32 rounded-xl mb-2 flex cursor-pointer items-center justify-center flex-col p-4 text-primary;">
+                <span class="text-[#6a6d78] text-base sm:text-lg lg:text-xl text-center mb-auto mt-36">Sembarang</span>
+            </div>
+            <h2 class="font-medium text-lg lg:text-2xl mt-1">Ragil</h2>
+        </div>
+        <div class="border snap-start xl:h-[342px] lg:h-72 md:h-64 sm:h-40 list-none bg-blue-100 rounded-xl flex cursor-pointer items-center justify-end flex-col text-primary pt-2 px-2 pb-4 xl:my-2 md:my-2 mx-2">
+            <div class="bg-white w-full xl:h-full lg:h-full md:h-[190px] sm:h-32 rounded-xl mb-2 flex cursor-pointer items-center justify-center flex-col p-4 text-primary;">
+                <span class="text-[#6a6d78] text-base sm:text-lg lg:text-xl text-center mb-auto mt-36">Sembarang</span>
+            </div>
+            <h2 class="font-medium text-lg lg:text-2xl mt-1">Ragil</h2>
+        </div>
+        <div class="border snap-start xl:h-[342px] lg:h-72 md:h-64 sm:h-40 list-none bg-blue-100 rounded-xl flex cursor-pointer items-center justify-end flex-col text-primary pt-2 px-2 pb-4 xl:my-2 md:my-2 mx-2">
+            <div class="bg-white w-full xl:h-full lg:h-full md:h-[190px] sm:h-32 rounded-xl mb-2 flex cursor-pointer items-center justify-center flex-col p-4 text-primary;">
+                <span class="text-[#6a6d78] text-base sm:text-lg lg:text-xl text-center mb-auto mt-36">Sembarang</span>
+            </div>
+            <h2 class="font-medium text-lg lg:text-2xl mt-1">Ragil</h2>
         </div>
     </div>
-    {{-- Kekono footer yo ga masalah,pokok e kek ono bg jok kosongan,kro Copyright --}}
+    </section>
+
+
 @endsection
